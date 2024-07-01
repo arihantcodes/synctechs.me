cfa54c7e133bafe88dfa37baf71037336978d301
itzamanjain <aman.044063@tmu.ac.in>
2024-07-01 11:49:04 +0530

    chore: Update theme colors and footer background color

--- layouts/partials/Footer.js
+++ layouts/partials/Footer.js
@@ -10,7 +10,7 @@ const Footer = () => {
   const { copyright, footer_content } = config.params;
   const { footer } = menu;
   return (
-    <footer className="section bg-theme-light pb-0">
+    <footer className="section bg-green-100 pb-0">
       <div className="container">
         {/* footer menu */}
         <div className="row">
